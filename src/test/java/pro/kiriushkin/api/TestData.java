26d7815fcfa5b46f0d05f9d5744b9e0d7c9274f8
Nika Dezhneva <132135996+Desinteger@users.noreply.github.com>
2023-08-22 16:16:39 +0300

    Update TestData.java

--- src/test/java/pro/kiriushkin/api/TestData.java
+++ src/test/java/pro/kiriushkin/api/TestData.java
@@ -3,5 +3,5 @@ package pro.kiriushkin.api;
 public class TestData {
     final static Integer modifiedCount = 1;
     static Order updatedOrder = new Order("Permanent exhibition", new Amount(0, 0),
-            "2023-08-12T11:41:04.703Z", "test1@test.com", "test1@test.com", "+79915678990");
+            "2023-08-12T11:41:04.703Z", "test1", "test1@test.com", "+79915678990");
 }
